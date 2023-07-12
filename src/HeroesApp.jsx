17adab44c2c206605a68a061104669f8e9ec4ed0
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2023-07-11 21:41:56 -0300

    navbar fix

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom'
-import { Navbar } from './ui/components/NavBar'
+import { Navbar } from './ui/components/Navbar'
 import { AuthProvider } from './auth/context/AuthProvider'
 
 const HeroesApp = () => {
